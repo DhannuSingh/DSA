c973714ea93486b4c354e131fa6296cb03824d14
Dhannu Singh <ds2839925@gmail.com>
2026-07-07 23:24:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (30.41%) - LeetSync

---
+++ 504-base-7/base-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if num == 0:
+            return "0"
+
+        is_negative = num < 0
+        num = abs(num)
+        res = []
+
+        while num > 0:
+            res.append(str(num % 7))
+            num //= 7
+        
+        if is_negative:
+            res.append("-")
+
+        return "".join(reversed(res))
